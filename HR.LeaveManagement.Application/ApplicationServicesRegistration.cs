519af66e6455b276f41efe5b3489913ad8252a9f
gabyslaw <gabyslaw@gmail.com>
2022-04-14 02:47:55 +0100

    Added automapper for mapping entities and added services registration

---
+++ HR.LeaveManagement.Application/ApplicationServicesRegistration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application
+{
+    public static class ApplicationServicesRegistration
+    {
+        public static void ConfigureApplicationServices(this IServiceCollection services)
+        {
+            // This makes multiple mapping profile possible without registering each mapping profile here
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        }
+    }
+}
