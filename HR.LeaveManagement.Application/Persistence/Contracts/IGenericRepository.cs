80e3d99e39627f3b40a66f7236f26a772500333e
gabyslaw <gabyslaw@gmail.com>
2022-04-14 02:22:36 +0100

    Added generic repository

--- HR.LeaveManagement.Application/Persistence/Contracts/IGenericRepository.cs
+++ HR.LeaveManagement.Application/Persistence/Contracts/IGenericRepository.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace HR.LeaveManagement.Application.Persistence.Contracts
 {
-    class IGenericRepository
+    public interface IGenericRepository<T> where T : class
     {
+        Task<T> Get(int id);
+        Task<IReadOnlyList<T>> GetAll();
+        Task<T> Add(T entity);
+        Task<T> Update(T entity);
+        Task<T> Delete(T entity);
+
+
     }
 }
--- HR.LeaveManagement.Application/Persistence/Contracts/ILeaveTypeRepository.cs
+++ HR.LeaveManagement.Application/Persistence/Contracts/ILeaveTypeRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HR.LeaveManagement.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace HR.LeaveManagement.Application.Persistence.Contracts
 {
-    class ILeaveTypeRepository
+    public interface ILeaveTypeRepository : IGenericRepository<LeaveType>
     {
     }
 }
